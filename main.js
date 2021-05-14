9f47e5b025ced68e3cf32d810ebe3c8540d6abbf
Carlito Murta <carlitomurtaramalho@gmail.com>
2021-05-13 22:00:49 -0300

    adjust the frame and removing menu

--- main.js
+++ main.js
@@ -1,39 +1,47 @@
-const { app, BrowserWindow } = require('electron');
-const url = require('url');
-const path = require('path');
+const { app, BrowserWindow, Menu } = require("electron");
+const url = require("url");
+const path = require("path");
 
 let win;
 
+const isMac = process.platform === "darwin";
+
 function createWindow() {
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
   win.loadURL(
     url.format({
       pathname: path.join(__dirname, `/dist/index.html`),
-      protocol: 'file:',
-      slashes: true
+      protocol: "file:",
+      slashes: true,
     })
   );
   // Open the DevTools.
   win.webContents.openDevTools();
 
-  win.on('closed', function() {
+  win.on("closed", function () {
     win = null;
   });
 }
 
-app.on('ready', createWindow);
-
-app.on('window-all-closed', function() {
-  if (process.platform !== 'darwin') app.quit();
+app.on("ready", function () {
+  createWindow();
+  Menu.setApplicationMenu(null); // Remove o menu
 });
 
-app.on('activate', function() {
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", function () {
   if (win === null) createWindow();
 });
+
+// JumpTaks (Com o botão direito na barra de tarefas do windows, é possível acessar tarefas rápidas)
+app.setUserTasks([]);
